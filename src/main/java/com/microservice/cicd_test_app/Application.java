55386e5b91e67c998d45cadf45fa85cc15c7f00c
Sharad Durgawad <durgawad@gmail.com>
2020-07-13 11:43:53 +0530

    Update Application.java
    changed print statement

--- src/main/java/com/microservice/cicd_test_app/Application.java
+++ src/main/java/com/microservice/cicd_test_app/Application.java
@@ -3,6 +3,6 @@ package com.microservice.cicd_test_app;
 public class Application {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Hello World!" );
+	        System.out.println( "Hello, This is Sharad Durgawad, a DevOps, Cloud and Automation enthusiast !!!" );
 	    }
 }
